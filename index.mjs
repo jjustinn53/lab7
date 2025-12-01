55fed2e118e006a8db19b232da450061fcb3e52f
Justin Nguyen <160275981+jjustinn53@users.noreply.github.com>
2025-11-30 21:00:47 -0800

    Added login and logout feature, sessions

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 import express from "express";
 import mysql from "mysql2/promise";
+import session from "express-session";
+import bcrypt from "bcrypt";
 
 const app = express();
 
@@ -8,6 +10,15 @@ app.use(express.static("public"));
 
 //for Express to get values using POST method
 app.use(express.urlencoded({ extended: true }));
+
+app.set("trust proxy", 1);
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //setting up database connection pool
 const pool = mysql.createPool({
@@ -19,13 +30,53 @@ const pool = mysql.createPool({
   waitForConnections: true,
 });
 
+// Send authenticated data to browser
+app.use((req, res, next) => {
+  res.locals.authenticated = req.session?.authenticated || false;
+  next();
+});
+
 //routes
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home.ejs", {message: ""});
+});
+
+//login
+app.post("/login", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  let passHash = "";
+
+  let sql = `SELECT * 
+              FROM users
+              WHERE username = ?`;
+
+  const [data] = await pool.query(sql, [username]);
+
+  if (data.length > 0) {
+    passHash = data[0].password;
+  }
+
+  const match = await bcrypt.compare(password, passHash);
+
+  if (match) {
+    req.session.authenticated = true;
+    req.session.userID = data[0].userID;
+    res.redirect("/authors");
+  } else {
+    res.render("home.ejs", { message: "Invalid username or password." });
+  }
+});
+
+// Logout Route
+app.get("/logout", async (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 //Display form to update Author
-app.get("/updateAuthor", async (req, res) => {
+app.get("/updateAuthor", isAuthenticated, async (req, res) => {
   let authorId = req.query.id;
   let sql = `SELECT *,
               DATE_FORMAT(dob,'%Y-%m-%d') ISOdob,
@@ -36,7 +87,7 @@ app.get("/updateAuthor", async (req, res) => {
   res.render("updateAuthor.ejs", { authorInfo });
 });
 
-app.post("/updateAuthor", async (req, res) => {
+app.post("/updateAuthor", isAuthenticated, async (req, res) => {
   let authorId = req.body.authorId;
   let fName = req.body.fn;
   let lName = req.body.ln;
@@ -48,7 +99,7 @@ app.post("/updateAuthor", async (req, res) => {
 });
 
 //delete author
-app.post("/deleteAuthor", async (req, res) => {
+app.post("/deleteAuthor", isAuthenticated, async (req, res) => {
   console.log(req.body);
   let authorId = req.body.authorId;
   let sql = `DELETE FROM authors WHERE authorId = ?`;
@@ -58,7 +109,7 @@ app.post("/deleteAuthor", async (req, res) => {
   res.redirect("/authors");
 });
 
-app.post("/deleteQuote", async (req, res) => {
+app.post("/deleteQuote", isAuthenticated, async (req, res) => {
   console.log(req.body);
   let quoteId = req.body.quoteId;
   let sql = `DELETE FROM quotes WHERE quoteId = ?`;
@@ -68,7 +119,7 @@ app.post("/deleteQuote", async (req, res) => {
   res.redirect("/quotes");
 });
 
-app.get("/authors", async (req, res) => {
+app.get("/authors", isAuthenticated, async (req, res) => {
   let sql = `SELECT authorId, firstName,lastName
             FROM authors
             ORDER BY lastName`;
@@ -77,7 +128,7 @@ app.get("/authors", async (req, res) => {
 });
 
 //Display quotes and render quotes page
-app.get("/quotes", async (req, res) => {
+app.get("/quotes", isAuthenticated, async (req, res) => {
   let sql = `SELECT q.quote, q.quoteId, CONCAT(a.firstName, ' ',  a.lastName) AS name
             FROM quotes q
             JOIN authors a ON q.authorId = a.authorId
@@ -88,12 +139,12 @@ app.get("/quotes", async (req, res) => {
 });
 
 // Display form to add author
-app.get("/addAuthor", (req, res) => {
+app.get("/addAuthor", isAuthenticated,(req, res) => {
   res.render("addAuthor.ejs");
 });
 
 // Add author info to DB
-app.post("/addAuthor", async (req, res) => {
+app.post("/addAuthor", isAuthenticated, async (req, res) => {
   let firstName = req.body.fn;
   let lastName = req.body.ln;
   let dob = req.body.dob;
@@ -114,7 +165,7 @@ app.post("/addAuthor", async (req, res) => {
 });
 
 // Display form to add quote
-app.get("/addQuotes", async (req, res) => {
+app.get("/addQuotes", isAuthenticated, async (req, res) => {
   let authorsSql = `SELECT authorId, CONCAT(firstName, ' ', lastName) AS fullName FROM authors`;
   const [authorsRows] = await pool.query(authorsSql);
 
@@ -125,7 +176,7 @@ app.get("/addQuotes", async (req, res) => {
 });
 
 // Add quote to DB
-app.post("/addQuote", async (req, res) => {
+app.post("/addQuote", isAuthenticated, async (req, res) => {
   let quote = req.body.quote;
   let category = req.body.category;
   let author = req.body.author;
@@ -158,3 +209,16 @@ app.get("/dbTest", async (req, res) => {
 app.listen(3000, () => {
   console.log("Express server running");
 });
+
+//Middleware
+function isAuthenticated(req, res, next) {
+  if (!req.session.authenticated) {
+    if (req.url !== "/login") {
+      res.render("login", { message: "Please login to continue." });
+    } else {
+      res.render("login", { message: "" });
+    }
+  } else {
+    next();
+  }
+}
